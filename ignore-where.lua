14a4328bd5200eb28fbb94972abc7a4547bff0ac
Han-Gang <35141480+Han-Gang@users.noreply.github.com>
2019-01-30 09:16:47 +0800

    ignore where clause
    where clause

--- ignore-where.lua
+++ ignore-where.lua
@@ -18,6 +18,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 function nowhere(query)
 
     local ind_tblname = string.find(string.upper(query), "WHERE")
