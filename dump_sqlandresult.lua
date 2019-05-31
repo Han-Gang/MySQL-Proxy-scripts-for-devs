5b7e07b0b28cf79b3a5eef265a611b9281685a89
Han-Gang <35141480+Han-Gang@users.noreply.github.com>
2019-05-31 17:00:09 +0800

    Update dump_sqlandresult.lua

--- dump_sqlandresult.lua
+++ dump_sqlandresult.lua
@@ -1,4 +1,3 @@
-
 local log_file = 'mysql.log'
 
 local fh = io.open(log_file, "a+")
@@ -23,7 +22,7 @@ end
 
 function read_query_result(inj)
 -- print ("lkjljk")
- fh:write("query: " .. (inj.query ) .. "##END\n")
+ fh:write("===BEGIN===\nquery: " .. (inj.query ) .. "##END\n")
 -- fh:write("query-time: " .. (inj.query_time / 1000) .. "ms")
 -- fh:write("response-time: " .. (inj.response_time / 1000) .. "ms")
 -- if inj.id == 2 then
@@ -43,15 +42,29 @@ function read_query_result(inj)
  end
  fh:write("rowcnt:" .. row_count  .. "\n")
  if res.query_status == proxy.MYSQLD_PACKET_ERR then
-    fh:write("" .. res.raw:sub(10) .. "\n")
+    fh:write("ERRORA_MSG:" .. res.raw:sub(10) .. "\n")
     
  end
 
  if nil ~= inj.resultset.rows then
-   for row in inj.resultset.rows do
-      fh:write("" .. row[1] .. "\n")
+   local num_cols = string.byte(res.raw, 1)
+   local to_write = ""
+   if num_cols > 0 and num_cols < 255 then
+   fh:write("colcnti:" .. num_cols .. "\n")
+
+    for row in inj.resultset.rows do
+      to_write = ""
+      for i = 1,num_cols,1 do
+          to_write = to_write .. "|".. tostring(row[i])
+ --         fh:write("" .. tostring(row[i]) .. "|")
+
+      end
+      fh:write("" .. to_write .. "\n")
+    end  
    end
  end 
+ fh:write("===END===\n")
  fh:flush()
+
 
 end
